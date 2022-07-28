36f5b96c86fbd462630867cf66eeffd705967568
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-07-27 20:38:38 -0600

    filterby input text & button added

--- src/components/github-search/github-search.spec.tsx
+++ src/components/github-search/github-search.spec.tsx
@@ -2,14 +2,24 @@ import React from 'react'
 import {render, screen} from '@testing-library/react'
 import GithubSearch from './github-search'
 
+beforeEach(() => {
+    render(<GithubSearch />)
+})
+
 describe('when the GithubSearchPage is mounted', () => {
     it('must display the title', () => {
-        render(<GithubSearch />)
-
         expect(
             screen.getByRole('heading', {
                 name: /github repositories list/i,
             }),
+        ).toBeInTheDocument()
+    })
+    it('must be an input text with label "filter by" field', () => {
+        expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument()
+    })
+    it('must be a search button', () => {
+        expect(
+            screen.getByRole('button', {name: /search/i}),
         ).toBeInTheDocument()
     })
 })
--- src/components/github-search/github-search.tsx
+++ src/components/github-search/github-search.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import {Typography} from '@mui/material'
+import {Button, TextField, Typography} from '@mui/material'
 
 const GithubSearch = () => (
-    <Typography variant="h3" component="h1">
-        github repositories list
-    </Typography>
+    <React.Fragment>
+        <Typography variant="h3" component="h1">
+            github repositories list
+        </Typography>
+        <TextField
+            label="Filter by"
+            id="filter"
+            name="filter"
+            // value={name}
+            // helperText={formErrors.name}
+            // error={!!formErrors.name.length}
+            // onChange={handleOnChange}
+            // onBlur={handleBlur}
+            fullWidth
+        />
+        <Button>Search</Button>
+    </React.Fragment>
 )
 
 export default GithubSearch
