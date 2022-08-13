c148403d4f2aeaab3af4d0a0e0c2140c783f55f4
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-08-12 20:15:10 -0600

    test - login, email, pw and send button

--- src/components/auth/login.spec.tsx
+++ src/components/auth/login.spec.tsx
@@ -3,9 +3,15 @@ import {screen, render, fireEvent} from '@testing-library/react'
 
 import Login from './login'
 
+beforeEach(() => render(<Login />))
+
 describe('when login page is mounted', () => {
     it('must display the login title', () => {
-        render(<Login />)
         expect(screen.getByText(/login/i)).toBeInTheDocument()
+    })
+    it('must have a form with the following fields: email, password and a submit button', () => {
+        expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
+        expect(screen.getByRole('button', {name: /send/i})).toBeInTheDocument()
     })
 })
--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
 const Login = () => {
-    return <h1>Login</h1>
+    return (
+        <React.Fragment>
+            <h1>Login</h1>
+            <TextField label="email" id="email"></TextField>
+            <TextField
+                label="password"
+                id="password"
+                type="password"
+            ></TextField>
+            <Button>Send</Button>
+        </React.Fragment>
+    )
 }
 
 export default Login
