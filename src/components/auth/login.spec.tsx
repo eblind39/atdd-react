648b78f2d02cc859dd99b999a1fc28365e3fffbd
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-08-12 20:29:29 -0600

    test - validate required fields in login

--- src/components/auth/login.spec.tsx
+++ src/components/auth/login.spec.tsx
@@ -15,3 +15,19 @@ describe('when login page is mounted', () => {
         expect(screen.getByRole('button', {name: /send/i})).toBeInTheDocument()
     })
 })
+
+describe('when the user leaves empty fields and clicks the submit button', () => {
+    it('display required messages as the format: "The [field name] is required"', () => {
+        expect(
+            screen.queryByText(/the email is required/i),
+        ).not.toBeInTheDocument()
+        expect(
+            screen.queryByText(/the password is required/i),
+        ).not.toBeInTheDocument()
+        fireEvent.click(screen.getByRole('button', {name: /send/i}))
+        expect(screen.getByText(/the email is required/i)).toBeInTheDocument()
+        expect(
+            screen.getByText(/the password is required/i),
+        ).toBeInTheDocument()
+    })
+})
--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, {SyntheticEvent, useState} from 'react'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 
 const Login = () => {
+    const [emailValMsg, setEmailValMsg] = useState<string>('')
+    const [passwordValMsg, setPasswordValMsg] = useState<string>('')
+
+    const handleSubmit = (evt: SyntheticEvent) => {
+        evt.preventDefault()
+        setEmailValMsg('The email is required')
+        setPasswordValMsg('The password is required')
+    }
+
     return (
         <React.Fragment>
             <h1>Login</h1>
-            <TextField label="email" id="email"></TextField>
-            <TextField
-                label="password"
-                id="password"
-                type="password"
-            ></TextField>
-            <Button>Send</Button>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    label="email"
+                    id="email"
+                    helperText={emailValMsg}
+                ></TextField>
+                <TextField
+                    label="password"
+                    id="password"
+                    type="password"
+                    helperText={passwordValMsg}
+                ></TextField>
+                <Button type="submit">Send</Button>
+            </form>
         </React.Fragment>
     )
 }
