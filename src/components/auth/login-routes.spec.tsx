2a6f95d3fad3bb48887c51fb5b7aa7e9e51c9161
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-08-14 14:13:39 -0600

    refactor: single navigation with renderWithRouter fn

--- src/components/auth/login-routes.spec.tsx
+++ src/components/auth/login-routes.spec.tsx
@@ -1,32 +1,18 @@
 import React from 'react'
-import {render, screen} from '@testing-library/react'
-import {BrowserRouter} from 'react-router-dom'
+import {screen} from '@testing-library/react'
 import App from '../../App'
+import {renderWithRouter} from '../../utils'
 
 describe('when the user is not authenticated and enters on admin page', () => {
     it('must be redirected to login page', () => {
-        window.history.pushState({}, '', '/admin')
-
-        render(
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>,
-        )
-
+        renderWithRouter(<App />, {route: '/admin'})
         expect(screen.getByText(/login page/i)).toBeInTheDocument()
     })
 })
 
 describe('when the user is not authenticated and enters on employee page', () => {
     it('must be redirected to login page', () => {
-        window.history.pushState({}, '', '/employee')
-
-        render(
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>,
-        )
-
+        renderWithRouter(<App />, {route: '/employee'})
         expect(screen.getByText(/login page/i)).toBeInTheDocument()
     })
 })
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,28 @@
-import ElegibleType from "../types/ElegibleType";
+import {BrowserRouter} from 'react-router-dom'
+import {render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import ElegibleType from '../types/ElegibleType'
 
-export const getElegibleTypeDescr = (elegibleType: ElegibleType | undefined) => {
-    if (typeof elegibleType === 'undefined') return 'No value given';
+const getElegibleTypeDescr = (elegibleType: ElegibleType | undefined) => {
+    if (typeof elegibleType === 'undefined') return 'No value given'
     switch (elegibleType) {
-      case ElegibleType.EQUALORUNDER_25: return 'Equal or under 25';
-      case ElegibleType.OVER_25: return 'Over 25';
+        case ElegibleType.EQUALORUNDER_25:
+            return 'Equal or under 25'
+        case ElegibleType.OVER_25:
+            return 'Over 25'
     }
-  }
+}
+
+const renderWithRouter = (
+    ui: React.ReactElement<any, string | React.JSXElementConstructor<any>>,
+    {route = '/'} = {},
+) => {
+    window.history.pushState({}, 'Test page', route)
+
+    return {
+        user: userEvent.setup(),
+        ...render(ui, {wrapper: BrowserRouter}),
+    }
+}
+
+export {getElegibleTypeDescr, renderWithRouter}
